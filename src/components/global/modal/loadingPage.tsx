7942bae1b18ccf61a34eada372551f36856d2ab1
lutfir2019 <lutfirrahman2019@gmail.com>
2024-05-04 01:10:05 +0700

    change spinner animation

--- src/components/global/modal/loadingPage.tsx
+++ src/components/global/modal/loadingPage.tsx
@@ -2,14 +2,14 @@
 
 import { useLoading } from "@/hook/useLoading";
 import Modal from "./modal";
-import { ImSpinner9 } from "react-icons/im";
+import { ImSpinner2 } from "react-icons/im";
 
 const LoadingPage = () => {
   const { data } = useLoading();
   return (
     <Modal open={data}>
       <div className="bg-slate-100 p-3 rounded-md">
-        <ImSpinner9 className="animate-spin" color="#020617" size={50} />
+        <ImSpinner2 className="animate-spin" color="#020617" size={50} />
       </div>
     </Modal>
   );
