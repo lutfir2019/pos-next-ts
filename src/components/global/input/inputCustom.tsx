2ab163673528c0571e6c1d24f331f908d80eec22
lutfir2019 <lutfirrahman2019@gmail.com>
2024-07-14 21:34:33 +0700

    update button create prodcut

--- src/components/global/input/inputCustom.tsx
+++ src/components/global/input/inputCustom.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useField } from "formik";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -39,7 +39,7 @@ const InputCustom: React.FC<Props> = ({
         disabled={disabled}
         placeholder={placeholder}
         {...field}
-        className="disabled:bg-slate-200 w-[100%] dark:disabled:bg-slate-900 text-black dark:text-white"
+        className="disabled:bg-slate-200 w-[100%] dark:disabled:bg-slate-900 text-black dark:text-white placeholder:text-black placeholder:dark:text-white"
       />
       {meta.touched && meta.error && (
         <span className="text-red-500">{meta.error}</span>
